95811f36b46bc100910d178672bc47dee6d673a0
Andrey Yamburg <andreyamburg@gmail.com>
2022-07-23 21:30:13 -0700

    Use vite

--- src/Hover.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Hover from './Hover';
-
-test('renders learn react link', () => {
-  render(<Hover />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import { defineManifest } from "@crxjs/vite-plugin";
+
+const manifest = defineManifest({
+  "short_name": "User Checker",
+  "name": "User Checker",
+  "version": "0.1.0",
+  "manifest_version": 3,
+  "permissions": [
+    "storage",
+    "tabs",
+    "activeTab"
+  ],
+  "content_scripts": [
+    {
+      "matches": ["https://*.reddit.com/*"],
+      "run_at": "document_end",
+      "js": ["src/main.jsx"]
+    }
+  ]
+});
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest })]
+})
