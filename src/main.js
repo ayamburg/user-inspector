7f45b0ef931154684e2dfabbb0f5ad0f089d74f3
Andrey Yamburg <andreyamburg@gmail.com>
2022-07-26 02:57:20 -0700

    Use reddit api pagination

--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
 import axios from 'axios';
 
+let seenAuthors = {};
+
+async function getCommentSubreddits(author){
+  const subreddits = {};
+  let after;
+  do{
+    const response = await axios.get(`/user/${author}/comments.json?limit=100&after=${after}`);
+    console.log(response);
+    const comments = response.data.data.children;
+    after = response.data.data.after;
+    console.log(comments);
+    comments.forEach(com => {
+      const sub = com.data.subreddit;
+      if(subreddits[sub]){
+        subreddits[sub] += 1;
+      }else{
+        subreddits[sub] = 1;
+      }
+    });
+  }while(after !== null);
+  return subreddits;
+}
+
 async function handleHover(event) {
   const author = event.fromElement.getElementsByClassName('author')[0].href.split("/").pop();
   console.log(author);
-  const response = await axios.get(`/user/${author}/comments.json?limit=100`);
-  console.log(response);
-  const comments = response.data.data.children;
-  console.log(comments);
-  const subreddits = {};
-  comments.forEach(com => {
-    const sub = com.data.subreddit;
-    if(subreddits[sub]){
-      subreddits[sub] += 1;
-    }else{
-      subreddits[sub] = 1;
-    }
-  });
-  console.log(subreddits);
+  if(!seenAuthors[author]){
+    seenAuthors[author] = 1;
+    seenAuthors[author] = await getCommentSubreddits(author);
+  }
+
+  const subreddits = seenAuthors[author];
+  console.log(seenAuthors);
   const topSubreddits = Object.keys(subreddits).sort((a, b) => subreddits[b] - subreddits[a]).slice(0,5);
   let authorToolTipHead;
   const interval = setInterval(function(){
