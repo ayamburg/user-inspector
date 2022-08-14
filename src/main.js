3b264f3e88299b50ae423c7c253d191d4bab9da7
Andrey Yamburg <andreyamburg@gmail.com>
2022-08-14 04:28:55 -0700

    Add on click tooltips

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { createPopper } from '@popperjs/core';
 
 const seenAuthors = {};
 
@@ -51,10 +52,20 @@ async function handleHover(event) {
   }
 
   const subreddits = seenAuthors[author].commentSubreddits;
+  Object.keys(seenAuthors[author].postSubreddits).forEach(sub => {
+    if(subreddits[sub]) subreddits[sub] += seenAuthors[author].postSubreddits[sub];
+    else subreddits[sub] = seenAuthors[author].postSubreddits[sub];
+  });
+  const total = Object.values(subreddits).reduce((a, b) => a + b);
+
   console.log(seenAuthors);
   const topSubreddits = Object.keys(subreddits)
     .sort((a, b) => subreddits[b] - subreddits[a])
     .slice(0, 5);
+
+  const topSubredditInfo = topSubreddits.map(sub => {
+    return {name: sub, contributions: subreddits[sub], percent: subreddits[sub]/total}
+  });
   let authorToolTipHead;
   const interval = setInterval(function () {
     const authorToolTipHeads = Array.from(
@@ -66,13 +77,43 @@ async function handleHover(event) {
     });
     if (authorToolTipHead) {
       clearInterval(interval);
-      authorToolTipHead.after(`Top Subreddits: ${topSubreddits.join(", ")}`);
+      authorToolTipHead.after(`Top Subreddits: ${topSubredditInfo.join(", ")}`);
     }
   }, 1000);
 }
 
 const authors = document.querySelectorAll(`a[href*="/user/"]`);
+const searchIcon = chrome.runtime.getURL("search-icon.png");
 
-authors.forEach((author) => {
-  author.addEventListener("mouseover", handleHover);
+authors.forEach((author, i) => {
+  const inspectHTML = `
+    <img class="userInspector-inspect-user" id="${i}" src="${searchIcon}" height="12">
+    <div class="userInspector-tooltip" id="userInspector-tooltip${i}">
+      <span >Tooltip text</span>
+    </div>
+  `;
+  author.insertAdjacentHTML("afterend", inspectHTML);
+  // author.addEventListener("mouseover", handleHover);
 });
+
+const inspectors = document.querySelectorAll(`.userInspector-inspect-user`);
+inspectors.forEach((inspector) => {
+  const tooltip = document.querySelector(`#userInspector-tooltip${inspector.id}`);
+  // TODO make this more performant by only updating poppers that are on screen https://popper.js.org/docs/v2/tutorial/
+  createPopper(inspector, tooltip);
+});
+
+document.addEventListener('click', function(e) {
+  if(e.target.classList.contains("userInspector-inspect-user")){
+    const tooltip = document.querySelector(`#userInspector-tooltip${e.target.id}`);
+    if(tooltip.hasAttribute('data-show'))
+      tooltip.removeAttribute('data-show');
+    else 
+      tooltip.setAttribute('data-show', '');
+  }else{
+    const activeTooltip = document.querySelector('.userInspector-tooltip[data-show]');
+    if (activeTooltip && !activeTooltip.contains(e.target)){
+      activeTooltip.removeAttribute('data-show');
+    }
+  }
+}); 
